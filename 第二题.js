0af4937b1b55dc01df56117a319b10fdc0b93dea
wanquanojbk <32610697+ojbk@users.noreply.github.com>
2017-10-08 22:12:08 +0800

    Add files via upload

---
+++ 第二题.js
@@ -0,0 +1,31 @@
+﻿var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+
+
+http.createServer( function (request, response) {
+    
+    var pathname = url.parse(request.url).pathname;
+
+    console.log("Request for " + pathname + " received.");
+
+    fs.readFile('./index.html', function (err, data) {
+        if (err) {
+            console.log(err);
+          
+            response.writeHead(404, {'Content-Type': 'text/html'});
+        }else{
+            
+            response.writeHead(200, {'Content-Type': 'text/html'});
+
+           
+            response.write(data.toString());
+        }
+        
+        response.end();
+    });
+}).listen(8081);
+
+
+console.log('Server running at http://localhost:8081/');
